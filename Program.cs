2c166ea3cd0c1c25e9ffdccbe0746fca8b653688
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-19 10:59:27 +0530

    [Rithesh]add:demo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number a:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number b:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum =x + y;
+            Console.WriteLine("sum is:" + sum);
+
+        }
+    }
+}
